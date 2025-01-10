8ba4bcdd11bcbdfe132e043e1ff32dc2280f9296
hoaxed <thiagomartinez960@gmail.com>
2025-01-10 19:27:03 -0300

    First step to changing pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 const langDropMenu = document.querySelector("#dropdownMenu");
 const menuOpt = document.querySelector("#menuOptions");
 const langBtns = document.querySelectorAll(".langBtns");
-
+const navOpts = document.querySelectorAll(".navOpts");
 let translations; // Define the translations variable
+let file;
+
+navOpts.forEach((opt) => {
+  opt.addEventListener("click", async () => {
+    navOpts.forEach((item) => item.classList.remove("active"));
+    opt.classList.add("active");
+
+    const id = opt.getAttribute("id");
+    console.log(id);
+
+    async function getFile() {
+      const response = await fetch(`./subpages/${id}.html`);
+      const text = await response.text();
+      return text;
+    }
+
+    file = await getFile();
+    console.log(file);
+  });
+});
 
 // Function to load translations
 async function loadTranslations() {
@@ -37,13 +57,15 @@ async function setLanguage(language) {
     await loadTranslations();
   }
   localStorage.setItem("language", language);
-  document.getElementById("whoTitle").innerHTML = translations[language].whoTitle;
+  document.getElementById("whoTitle").innerHTML =
+    translations[language].whoTitle;
   document.getElementById("whoDesc").innerHTML = translations[language].whoDesc;
 }
 
 // Set the saved language when the page loads
 document.addEventListener("DOMContentLoaded", async () => {
   await loadTranslations();
-  const savedLanguage = localStorage.getItem("language") || navigator.language.split("-")[0];
+  const savedLanguage =
+    localStorage.getItem("language") || navigator.language.split("-")[0];
   setLanguage(savedLanguage in translations ? savedLanguage : "en");
 });
